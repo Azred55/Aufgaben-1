2f2e6643e533fade6f8b9d6e02f126e125d09e15
Deniz <mail>
2025-08-28 10:53:56 +0200

    3.5.3 2

---
+++ 3.5.3Aufgabe2.dart
@@ -0,0 +1,9 @@
+int countChars(String text) {
+  return text.length;
+}
+
+void main() {
+  print("Anzahl der Zeichen: ${countChars("Bye")}");
+  print("Anzahl der Zeichen: ${countChars("Deniz")}");
+  print("Anzahl der Zeichen: ${countChars("Feierabend")}");
+}
