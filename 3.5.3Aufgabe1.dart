40853b11e47d65e6b1672ef8343ee9c798ac57a0
Deniz <mail>
2025-08-28 10:47:40 +0200

    3.5.3 1

---
+++ 3.5.3Aufgabe1.dart
@@ -0,0 +1,9 @@
+int sumThree(int a, int b, int c) {
+  return a + b + c;
+}
+
+void main() {
+  print(sumThree(1, 2, 3)); // 6
+  print(sumThree(10, 20, 30)); // 60
+  print(sumThree(-5, 15, 5));
+}
