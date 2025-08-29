1dd060a2d0a37785222ba4199dde6ab9934abe89
Deniz <mail>
2025-08-29 11:37:43 +0200

    korrektur

--- Taschenrechner.dart
+++ Taschenrechner.dart
@@ -3,10 +3,10 @@ import 'dart:io';
 void main() {
   print('Hallo Taschenrechenr');
 
-  stdout.write('GIb deine Zahl ein');
+  stdout.write('Gib deine Zahl ein');
   String? ersteEingabe = stdin.readLineSync();
 
-  stdout.write('GIb deine zweite Zahl ein');
+  stdout.write('Gib deine zweite Zahl ein');
   String? zweiteEingabe = stdin.readLineSync();
 
   stdout.write('Operator eingeben');
